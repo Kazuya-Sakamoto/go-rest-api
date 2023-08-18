5baf7ded43188d9fa3f9304139a1910c272dd85f
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2023-08-18 17:20:47 +0900

    add DeleteDiaryComment (#5)
    * create diary model
    
    * add GetAllDiaries method
    
    * add methods in GetDiaryById, CreateDiary, UpdateDiary, DeleteDiary
    
    * create intermediate table for Diary and User relationship
    
    * add DeleteDiaryComment
    
    ---------
    
    Co-authored-by: 坂本一弥 <sakamotokazuya@sakamotokazuyanoMacBook-Pro.local>

--- repository/diary_comment_repository.go
+++ repository/diary_comment_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	"go-rest-api/model"
 
 	"gorm.io/gorm"
@@ -9,6 +10,7 @@ import (
 type IDiaryCommentRepository interface {
 	GetDiaryCommentsByDiaryIDAndUserID(diaryComments *[]model.DiaryComment, diaryId, userId uint) error
 	CreateDiaryComment(diaryComment *model.DiaryComment) error
+	DeleteDiaryComment(userId, diaryCommentId uint) error
 }
 
 type diaryCommentRepository struct {
@@ -34,6 +36,17 @@ func (dcr *diaryCommentRepository) GetDiaryCommentsByDiaryIDAndUserID(diaryComme
 func (dcr *diaryCommentRepository) CreateDiaryComment(diaryComment *model.DiaryComment) error {
 	if err := dcr.db.Create(diaryComment).Error; err != nil {
 		return err
+	}
+	return nil
+}
+
+func (dcr *diaryCommentRepository) DeleteDiaryComment(userId, diaryCommentId uint) error {
+	result := dcr.db.Where("id=? AND user_id=?", diaryCommentId, userId).Delete(&model.DiaryComment{})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected < 1 {
+		return fmt.Errorf("object does not exist")
 	}
 	return nil
 }
